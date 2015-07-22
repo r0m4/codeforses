dd49bccc49c5a10f92b6d98a6b1cc6373be3121c
Roman <formicrotech@gmail.com>
2015-07-22 22:22:36 +0300

    last

--- 22.07.2015.2.js
+++ 22.07.2015.2.js
@@ -1,3 +1,40 @@
+/*Gerald bought two very rare paintings at the Sotheby's auction and he now wants to hang them on the wall. For that he bought a special board to attach it to the wall and place the paintings on the board. The board has shape of an a1 × b1 rectangle, the paintings have shape of a a2 × b2 and a3 × b3 rectangles.
+
+Since the paintings are painted in the style of abstract art, it does not matter exactly how they will be rotated, but still, one side of both the board, and each of the paintings must be parallel to the floor. The paintings can touch each other and the edges of the board, but can not overlap or go beyond the edge of the board. Gerald asks whether it is possible to place the paintings on the board, or is the board he bought not large enough?
+
+Input
+The first line contains two space-separated numbers a1 and b1 — the sides of the board. Next two lines contain numbers a2, b2, a3 and b3 — the sides of the paintings. All numbers ai, bi in the input are integers and fit into the range from 1 to 1000.
+
+Output
+If the paintings can be placed on the wall, print "YES" (without the quotes), and if they cannot, print "NO" (without the quotes).
+
+Sample test(s)
+input
+3 2
+1 3
+2 1
+output
+YES
+input
+5 5
+3 3
+3 3
+output
+NO
+input
+4 2
+2 3
+1 2
+output
+YES
+*/
+
+
+
+
+
+
+
 var board = [8,8];
 var fRect = [6,4];
 var sRect = [7,5];
